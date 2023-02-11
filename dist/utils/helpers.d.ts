2563130ee5c16883a3da553b1e7f218f63d458c1
Mohamad Adithya <mohamadadithya@gmail.com>
2023-02-11 16:31:17 +0700

    add declare types

---
+++ dist/utils/helpers.d.ts
@@ -0,0 +1,3 @@
+import type { Render, HolidayClassHandler } from "./types";
+export declare const render: (args: Render) => void;
+export declare const getHolidayClassHandler: (args: HolidayClassHandler) => string;
---
+++ dist/utils/types.d.ts
@@ -0,0 +1,38 @@
+export interface Date {
+    disabled: boolean;
+    date: string;
+}
+interface Lang {
+    code?: string;
+    ui?: {
+        quickActions?: {
+            today?: string;
+            tomorrow?: string;
+            inTwoDays?: string;
+        };
+    };
+    months?: string[];
+    weekdays?: string[];
+}
+export interface Render {
+    container: HTMLAreaElement;
+    dates: Date[];
+    months: string[];
+    years: string[];
+    nowDay: string;
+    nowMonth: string;
+    quickActions: boolean;
+    locale: Locale;
+}
+export interface Locale {
+    format: string;
+    months?: string[];
+    weekdays?: string[];
+    lang: Lang;
+}
+export interface HolidayClassHandler {
+    date: string;
+    nowMonth: string;
+}
+export type ExpandedMode = "months" | "years";
+export {};
